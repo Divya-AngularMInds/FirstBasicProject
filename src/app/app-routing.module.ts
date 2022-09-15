d00f70de77772d428e99972483c108218f30f526
Divya-Khivansara <divya@angularminds.in>
2022-09-15 12:51:42 +0530

    page commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BooksComponent } from './books/books/books.component';
+
+const routes: Routes = [
+  { path: '', component: BooksComponent },
+  { path: 'books', component: BooksComponent },
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/books/book/book.component.ts
+++ src/app/books/book/book.component.ts
@@ -13,23 +13,23 @@ export class BookComponent implements OnInit {
   @Input() book: any = {};
   @Output() cartValue = new EventEmitter<Book>();
   @Output() childEvent = new EventEmitter();
-  constructor(private cartService: CartService, private ls: BooksService, private route:Router) { }
+  constructor(private cartService: CartService, private ls: BooksService, private route: Router) { }
   isInCart: boolean = false;
 
   ngOnInit(): void {
     this.book.count = 0;
   }
-  total:number=0;
-    addToCart(price:any){
-      this.isInCart = true;
-      this.total += 1;
-      this.book.count += 1;
-      if (this.book.count < 2) {
-        this.cartService.add(this.book);
-        
-        this.childEvent.emit(this.cartService.cart.length);
-      }
-      this.ls.totalCart = this.ls.totalCart + price;
+  total: number = 0;
+  addToCart(price: any) {
+    this.isInCart = true;
+    this.total += 1;
+    this.book.count += 1;
+    if (this.book.count < 2) {
+      this.cartService.add(this.book);
+
+      this.childEvent.emit(this.cartService.cart.length);
+    }
+    this.ls.totalCart = this.ls.totalCart + price;
   }
   removeFromCart(price: any) {
     this.ls.totalCart = this.ls.totalCart - price;
@@ -38,11 +38,11 @@ export class BookComponent implements OnInit {
     this.book.count -= 1;
     if (this.book.count === 0) {
       this.cartService.remove(this.book);
-    this.childEvent.emit(this.cartService.cart.length);
+      this.childEvent.emit(this.cartService.cart.length);
     }
-    
-    
-    
+
+
+
   }
   // remove(index:any) {
   //   this.cartService.cart.splice(index, 1);
@@ -51,6 +51,6 @@ export class BookComponent implements OnInit {
     this.ls.showBook = this.book
     this.route.navigate(['./page']);
   }
-  
+
 }
-export function removeFromCart(){}
+export function removeFromCart() { }
